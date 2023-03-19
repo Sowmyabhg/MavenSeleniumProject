7168b54178fb9e2865f9a3da2e1055ed7d74c4d0
SowmyaBilasanur <88008152+SowmyaBilasanur@users.noreply.github.com>
2023-03-19 01:05:38 +0100

    FirstCommit

---
+++ MyProject/src/MyTest/Test1.java
@@ -0,0 +1,25 @@
+package MyTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+	WebDriver driver;
+	
+	@Test
+	public void setup()
+	{
+		   System.setProperty("webdriver.http.factory", "jdk-http-client");
+		   WebDriverManager.chromedriver().setup();
+	        WebDriver driver=new ChromeDriver();
+	        driver.manage().window().maximize();
+	        driver.get("https://amazon.in");
+	        
+	        driver.quit();
+	}
+	
+
+}
